0c32069d3c7d3365b81de3cfe034cdbf4935b906
ayomideajayi <ayomideseaz@gmail.com>
2021-06-25 01:43:11 +0100

    new commit

--- README.md
+++ README.md
@@ -2,11 +2,65 @@
 
 a youtube tutorial tourism app
 
-## Getting Started
+## Splash Screen
 
-This project is a starting point for a Flutter application.
+WIDGETS
 
-A few resources to get you started if this is your first Flutter project:
+SafeArea: 
+- to prevent rounded corners or notches of the device from biting into our content thereby complicating layouts
+- to prevent notifications creeping into the app screen 
+
+Scaffold: 
+- Provides many widgets or APIs like Drawer, snackbar, bottomNavBar, FloatingActionButton, AppBar, etc
+- Occupies the whole screen
+
+Container: 
+- Helps you compose, decorate and postion child widget.
+
+SizedBox
+- Is a widget with a specific size that directs its child to have that same size
+- Great for adding gaps between widgets
+
+###############################################################
+
+PROPERTIES
+
+decoration:
+- to add shape to container
+
+margin:
+- adds an empty space that surrounds the widget
+
+padding: 
+- to add an empty space between the child widget and the container boundary
+
+alignment:
+- to align child widget to the center of the parent
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 - [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -6,102 +6,107 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
       return Scaffold(
         body: SafeArea(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/splash_bg.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
             child: Container(
-              padding: EdgeInsets.only(left: 30.0, top:30.0, right:30.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/splash_bg.png'),
+                    fit: BoxFit.cover,
+                  )
+              ),
 
-                      Text('Tx.',
+              child: Container(
+                padding: EdgeInsets.only(left: 30.0, top: 30.0, right: 30.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Tx.',
                           style: TextStyle(
-                              color: Color(0xffc40000),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30.0
-                          )),
-                      Text('Skip',
+                            color: Color(0xffc40000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30.0
+                          ),
+                        ),
+                        Text(
+                          'Skip',
                           style: TextStyle(
                               color: kPrimaryColor,
                               fontSize: 20.0
-                          ))
-                    ],
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 48.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Welcome',
-                            style: TextStyle(
-                                color: kPrimaryColor,
-                                fontSize: 50.0
-                            )),
-                        Row(
-                          children: [
-                            Text('to',
-                                style: TextStyle(
-                                    color: kPrimaryColor,
-                                    fontSize: 50.0
-                                )),
-                            Text('TravelX',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: kPrimaryColor,
-                                    fontSize: 50.0
-                                ))
-                          ],
-                        ),
-                        SizedBox(
-                          width: 400,
-                          height: 500,
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                  child: Container(
-                                    margin: EdgeInsets.only(top:60),
-                                    decoration: BoxDecoration(
-                                        color: Color(0xffC40000),
-                                        borderRadius: BorderRadius.circular(12.0)
-                                    ),
-                                    child: Icon(
-                                        Icons.keyboard_arrow_right,
-                                        color: Colors.white
-                                    ),
-                                  )),
-                              Positioned(
-                                left: 0,
-                                right: -120,
-                                top: -30,
-                                bottom: 0,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      image: AssetImage('assets/images/Travelers-img.png'),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
                           ),
-                        ),
+                        )
                       ],
                     ),
-                  )
-                ],
+                    Container(
+                      padding: EdgeInsets.only(top:40.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Welcome',
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontSize: 50.0
+                          )),
+                          Row(
+                            children: [
+                              Text('to',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: kPrimaryColor,
+                                  fontSize: 50.0
+                                )),
+                              SizedBox(width: 5.0),
+                              Text('TravelX',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: kPrimaryColor,
+                                fontSize: 50.0
+                              ))
+                            ]
+                          ),
+                          SizedBox(
+                            width: 400,
+                            height: 500,
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  child: Container(
+                                    margin: EdgeInsets.only(top: 60),
+                                    decoration: BoxDecoration(
+                                      color: Color(0xffc40000),
+                                      borderRadius: BorderRadius.circular(12.0)
+                                    ),
+                                    child: Icon(
+                                      Icons.keyboard_arrow_right,
+                                      color: Colors.white
+                                    ),
+                                  )
+                                ),
+                                Positioned(
+                                  left: 0,
+                                  right: -120,
+                                  top: -30,
+                                  bottom: 0,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: AssetImage('assets/images/Travelers-img.png'),
+                                        fit: BoxFit.cover
+                                      )
+                                    )
+                                  ),
+                                )
+                              ],
+                            )
+                          )
+                        ]
+                      )
+                    )
+                  ],
+                ),
               ),
-            ),
-          ),
+            )
         ),
       );
-
   }
 }
